6b3953708f7533d36aba8dc4082ac764c6643720
Trần Lượng <142708624+luongtrz@users.noreply.github.com>
2025-07-01 04:07:53 +0000

    update python

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
+def normalize_phone_number(phone):
+    phone = phone.replace('.', '').replace('-', '')
+    
+    if phone.startswith('0'):
+        phone = phone[1:]
+    return '+84' + phone[:3] + ' ' + phone[3:6] + ' ' + phone[6:]
 
-int sum(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Sum: " << sum(3, 5) << std::endl;
-    return 0;
-}
+# Kiểm thử
+if __name__ == "__main__":
+    print("Kết quả:", normalize_phone_number("0123.456.789"))
